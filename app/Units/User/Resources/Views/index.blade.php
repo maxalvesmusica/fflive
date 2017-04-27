abc81a3bc1f251276a1c0a1b5974a62b76f9406b
Jammerson Muniz <jammersonmuniz@Jammersons-MacBook-Pro.local>
2017-04-27 18:53:54 -0300

    start count bets

--- app/Units/User/Resources/Views/index.blade.php
+++ app/Units/User/Resources/Views/index.blade.php
@@ -4,15 +4,16 @@
     <div class="row">
         <div class="img-pormocao-home"><img src="{{url('img/home.jpg')}}"></div>
     </div>
+    <!--
     <div class="row">
-        <div class="img-pormocao-home2"><a href="{{route('user.whats', $user->id)}}"><img src="{{url('img/promowhats.jpg')}}"></a></div>
+        <div class="img-pormocao-home2"><a href="user.whats"><img src="{{url('img/promowhats.jpg')}}"></a></div>
     </div>
 
     <div class="row">
-        <div class="img-pormocao-home2"><a href="{{route('user.insta', $user->id)}}"><img src="{{url('img/promoinsta.jpg')}}"></a></div>
+        <div class="img-pormocao-home2"><a href="user.insta"><img src="{{url('img/promoinsta.jpg')}}"></a></div>
     </div>
 
     <div class="row">
-        <div class="img-pormocao-home2"><a href="{{route('user.face', $user->id)}}"><img src="{{url('img/promoface.jpg')}}"></a></div>
-    </div>
+        <div class="img-pormocao-home2"><a href="user.face><img src="{{url('img/promoface.jpg')}}"></a></div>
+    </div>-->
 @stop
