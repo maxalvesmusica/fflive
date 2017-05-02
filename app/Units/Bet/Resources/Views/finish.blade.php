065ec03c75bdea002e2477825871ea68266c1a2a
Jammerson Muniz <jammersonmuniz@Jammersons-MacBook-Pro.local>
2017-05-01 21:05:19 -0300

    improvements

--- app/Units/Bet/Resources/Views/finish.blade.php
+++ app/Units/Bet/Resources/Views/finish.blade.php
@@ -4,7 +4,7 @@
         <div class="col-md-12">
         	<h3>Promoção válida apenas pra quem compartilhar essa imagem em modo publico no seu Facebook</h3>
             <div class="col-md-12">
-                <iframe src="https://www.facebook.com/plugins/share_button.php?href={{url('img/'.$img->link)}}&layout=button&size=large&mobile_iframe=true&width=119&height=28&appId" width="119" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <iframe src="https://www.facebook.com/plugins/share_button.php?m2w&s=100&href={{url('img/'.$img->link)}}&layout=button&size=large&mobile_iframe=true&width=119&height=28&appId" width="119" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
             </div>
             <div class="col-md-12">
                 <a href="{{ route('transmission.user')}}" class="btn btn-info">Confirmar</a>
