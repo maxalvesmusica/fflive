fb0d566870b0c651c5c4cd805b88f76fc0958701
Jammerson Muniz <jammersonmuniz@Jammersons-MacBook-Pro.local>
2017-05-02 06:22:00 -0300

    improvements

--- app/Units/Bet/Resources/Views/finish.blade.php
+++ app/Units/Bet/Resources/Views/finish.blade.php
@@ -4,7 +4,7 @@
         <div class="col-md-12">
         	<h3>Promoção válida apenas pra quem compartilhar essa imagem em modo publico no seu Facebook</h3>
             <div class="col-md-12">
-                <div id="shareBtn" class="btn btn-success clearfix">Compartilhar</div>
+                <div id="shareBtn" class="btn btn-success clearfix" style="background-color: #3B5998"><i class="fa fa-facebook"></i> Compartilhar</div>
                 <a href="{{ route('transmission.user')}}" class="btn btn-info">Confirmar</a>
             </div>
         	<div class="thumbnail">
