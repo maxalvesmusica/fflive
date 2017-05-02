fdbd6ba65a6b2e576c1ebd6cf6e62f52f22f604e
Jammerson Muniz <jammersonmuniz@Jammersons-MacBook-Pro.local>
2017-05-02 06:12:22 -0300

    improvements

--- app/Units/Bet/Resources/Views/finish.blade.php
+++ app/Units/Bet/Resources/Views/finish.blade.php
@@ -4,13 +4,24 @@
         <div class="col-md-12">
         	<h3>Promoção válida apenas pra quem compartilhar essa imagem em modo publico no seu Facebook</h3>
             <div class="col-md-12">
-                <iframe src="https://www.facebook.com/plugins/share_button.php?href={{url('img/'.$img->link)}}&layout=button&size=large&mobile_iframe=true&width=119&height=28&appId" width="119" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-            </div>
-            <div class="col-md-12">
+                <div id="shareBtn" class="btn btn-success clearfix">Compartilhar</div>
                 <a href="{{ route('transmission.user')}}" class="btn btn-info">Confirmar</a>
             </div>
         	<div class="thumbnail">
         		<img src="{{url('img/'.$img->link)}}" alt="">
         	</div>	
         </div>
+@stop
+
+@section('scripts')
+<script>
+document.getElementById('shareBtn').onclick = function() {
+  FB.ui({
+    method: 'share',
+    display: 'popup',
+    mobile_iframe: true,
+    href: '{{url('img/'.$img->link)}}',
+  }, function(response){});
+}
+</script>
 @stop
